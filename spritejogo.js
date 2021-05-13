f9c3d33f7ae591e44491221f7a6b79be435d6cea
ruansotovia <61193530+ruansotovia@users.noreply.github.com>
2021-05-12 21:13:17 -0300

    Second Version

--- spritejogo.js
+++ spritejogo.js
@@ -6,11 +6,11 @@ function Sprite(x, y, largura, altura) {
 
 	
 	this.desenha = function(xCanvas, yCanvas){
-		ctx.drawImage(img, this.x, this.y, this.largura, this.altura, xCanvas, yCanvas, this.largura, this.altura,);
+		ctx.drawImage(img, this.x, this.y, this.largura, this.altura, xCanvas, yCanvas, this.largura, this.altura);
 	}
 }
 
-var bg = new Sprite(0, 0, 1350, 600),
+var bg = new Sprite(0, 0, 2100, 700),
 spritePersonagem = new Sprite(48, 743, 112, 78),
 jogar = new Sprite(1037, 756, 232, 79),
 perdeu = new Sprite(395, 756, 378, 74);
